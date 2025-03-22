7c2e650bdb0aa6cba7d16ced9c470f272e516445
Piratang Zemoo <ariascyreljames@gmail.com>
2025-03-22 22:01:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
